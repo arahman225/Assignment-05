8f1faa46fb6eafb4cf489a9ce58a3eaf22e30fb9
arahman225 <upworkabdurrahman@gmail.com>
2024-09-22 23:58:40 +0600

    added navbar and blog section

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.getElementById('donation-1').addEventListener('click', function(){
+
+
+    const inputAmoutn = parseFloat(document.getElementById('input-amount').value);
+
+    if(inputAmoutn === 'number'){
+
+        
+    }
+})
